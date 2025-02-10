83bb96dedf1ad99d119e68dab55a589964269e36
jgo179 <jgo179@gmail.com>
2025-02-10 15:47:34 -0300

    chore: add prisma generate to prepare script

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -30,7 +30,10 @@ const TransactionsPage = async () => {
         </div>
 
         <ScrollArea>
-          <DataTable columns={transactionColumns} data={transactions} />
+          <DataTable
+            columns={transactionColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
         </ScrollArea>
       </div>
     </>
